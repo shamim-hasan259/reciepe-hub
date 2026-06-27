c83222745de84f002208cd820b17192fe5ffa9b4
shamim-hasan259 <mdshamimhasan791@gmail.com>
2026-06-27 14:45:12 +0600

    dismis report successfully

--- recipe-hub-client/src/lib/actions/report.js
+++ recipe-hub-client/src/lib/actions/report.js
@@ -1,5 +1,12 @@
 "use server";
-import { serverMution } from "../core/server";
+import { revalidatePath } from "next/cache";
+import { removeServer, serverMution } from "../core/server";
 export const postReprtRecipe = async (report) => {
   return await serverMution("/api/recipe/report", report, "POST");
+};
+
+export const dismisReport = async (id) => {
+  const res = await removeServer(`/api/delete/reoprt/${id}`, "DELETE");
+  revalidatePath("/dashboard/admin/reports");
+  return await res;
 };
